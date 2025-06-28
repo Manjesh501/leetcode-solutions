7a7519de3b4116c0acc35fab8b5ab2471788c336
Manjesh Tiwari <manjesht78@gmail.com>
2025-06-29 00:59:56 +0530

    Add solution for Find Subsequence of Length K With the Largest Sum
    - Runtime: N/A
    - Memory: N/A
    - Language: JavaScript
    - Difficulty: Easy

---
+++ solutions/find-subsequence-of-length-k-with-the-largest-sum.js
@@ -0,0 +1,14 @@
+/*
+ * LeetCode Problem: Find Subsequence of Length K With the Largest Sum
+ * Submission ID: sub_1751138995226
+ * Runtime: N/A
+ * Memory: N/A
+ * Submitted: 2025-06-28T19:29:55.228Z
+ * Difficulty: Easy
+ * Category: Unknown
+ */
+
+Input: nums = [2,1,3,3], k = 2
+Output: [3,3]
+Explanation:
+The subsequence has the largest sum of 3 + 3 = 6.
